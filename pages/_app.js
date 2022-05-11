e50adb07188543de2dad8c9ef0f227fff7bdc766
Anna Holtmann <anna_maria.heuer@mac.com>
2022-05-11 11:35:26 +0200

    Add Globalstyles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,43 @@
-import '../styles/globals.css';
+import { createGlobalStyle } from 'styled-components';
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<GlobalStyle />
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
+
+const GlobalStyle = createGlobalStyle`
+html,
+body {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+	background-color: #E2CADF;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+form {
+display: flex;
+
+flex-direction: column;
+gap: 10px;
+margin: 30px;
+
+}
+`;
--- pages/_documents.js
+++ pages/_documents.js
@@ -1,6 +1,35 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
+export default class MyDocument extends Document {
+	static getInitialProps({ renderPage }) {
+		const sheet = new ServerStyleSheet();
+
+		const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+
+		const styleTags = sheet.getStyleElement();
+		return { ...page, styleTags };
+	}
+	render() {
+		return (
+			<Html>
+				<Head>
+					{this.props.styleTags}
+					<link
+						rel="stylesheet"
+						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+/*export default function Document() {
 	return (
 		<Html>
 			<Head>
@@ -15,4 +44,4 @@ export default function Document() {
 			</body>
 		</Html>
 	);
-}
+}*/
