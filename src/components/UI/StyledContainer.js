453993fa72ab54a30e75efdcf357f88e0aea520b
Anna Holtmann <anna_maria.heuer@mac.com>
2022-05-16 21:13:05 +0200

    Updated stylecontainer width and height

--- src/components/UI/StyledContainer.js
+++ src/components/UI/StyledContainer.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 const Container = styled.div`
 	background-color: white;
-	max-width: 384px;
-	max-height: 436px;
+	width: 384px;
+	height: 436px;
 	padding: 20px 0 20px 20px;
 	display: flex;
 	border-radius: 50px;
