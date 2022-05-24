f8e80c45c05b3e28b0c0a183e48f923c340ded3f
Anna Holtmann <anna_maria.heuer@mac.com>
2022-05-24 17:35:31 +0200

    Added svgs

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,56 @@
 import Link from 'next/link';
+import styled from 'styled-components';
+import MySVG from '../UI/MySVG';
 
 export default function Footer() {
 	return (
-		<footer>
-			<Link passHref href="/products">
-				<button type="button">products</button>
-			</Link>
-			<Link passHref href="/create">
-				<button type="button">create</button>
-			</Link>
-			<Link passHref href="/profile">
-				<button type="button">profile</button>
-			</Link>
-			<Link passHref href="/">
-				<button type="button">start</button>
-			</Link>
-		</footer>
+		<FooterContainer>
+			<FooterNav>
+				<Link passHref href="/products">
+					<button type="button">
+						<SvgContainer>
+							<MySVG size="34" variant="products" />
+						</SvgContainer>
+					</button>
+				</Link>
+				<Link passHref href="/create">
+					<button type="button">
+						<SvgContainer>
+							<MySVG size="34" variant="add" />
+						</SvgContainer>
+					</button>
+				</Link>
+				<Link passHref href="/profile">
+					<button type="button">
+						<SvgContainer>
+							<MySVG size="34" variant="profile" />
+						</SvgContainer>
+					</button>
+				</Link>
+				<Link passHref href="/">
+					<button type="button">start</button>
+				</Link>
+			</FooterNav>
+		</FooterContainer>
 	);
 }
+
+const FooterContainer = styled.footer`
+	width: 100%;
+	background-color: var(--main-bg-color-dark);
+	position: sticky;
+	bottom: 0;
+`;
+
+const FooterNav = styled.nav`
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+`;
+
+const SvgContainer = styled.div`
+	background-color: var(--main-bg-color);
+	width: 55px;
+	border-radius: 50%;
+`;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,8 +3,8 @@ import Footer from '../Footer/Footer';
 export default function Layout({ children }) {
 	return (
 		<>
+			{children}
 			<Footer />
-			<div>{children}</div>
 		</>
 	);
 }
