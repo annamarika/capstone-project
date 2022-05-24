9014830ddb4eb9b64d83e7bac45589b75c2ee0c1
Anna Holtmann <anna_maria.heuer@mac.com>
2022-05-24 14:00:51 +0200

    Add layout to app page

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import GlobalStyle from '../src/components/UI/GlobalStyle';
+import Layout from '../src/components/Layout/Layout';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<GlobalStyle />
-			<Component {...pageProps} />
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
 		</>
 	);
 }
