5b4f6e4f9ec803ada161ae5cd77f3e479c16bc33
Anna Holtmann <anna_maria.heuer@mac.com>
2022-05-25 12:13:28 +0200

    Updated button storybook

--- src/components/Button/CancelButton.stories.js
+++ src/components/Button/CancelButton.stories.js
@@ -15,8 +15,35 @@ export default {
 	],
 };
 
-const ButtonText = 'Cancel';
+const CancelText = 'Cancel';
+
+const CreateText = 'Create';
+
+const DeleteText = 'Delete';
+
+const EditText = 'Edit';
+
+const EmailText = 'Email';
+
+const SaveText = 'Save';
 
 export function CancelButton() {
-	return <DefaultButton>{ButtonText}</DefaultButton>;
+	return <DefaultButton>{CancelText}</DefaultButton>;
+}
+
+export function CreateButton() {
+	return <DefaultButton>{CreateText}</DefaultButton>;
+}
+
+export function DeleteButton() {
+	return <DefaultButton>{DeleteText}</DefaultButton>;
+}
+export function EditButton() {
+	return <DefaultButton>{EditText}</DefaultButton>;
+}
+export function EmailButton() {
+	return <DefaultButton>{EmailText}</DefaultButton>;
+}
+export function SaveButton() {
+	return <DefaultButton>{SaveText}</DefaultButton>;
 }
