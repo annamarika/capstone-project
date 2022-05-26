ef2c02ae6566ad9536817f561447ece775beabda
Anna Holtmann <anna_maria.heuer@mac.com>
2022-05-26 15:39:31 +0200

    Adding upload button

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,16 +8,54 @@ import InputSingleContainer from '../UI/Form/StyledInputSingleContainer';
 import Label from '../UI/Form/StyledLable';
 import Input from '../UI/Form/StyledInput';
 import ButtonContainer from '../UI/Button/StyledButtonContainer';
+import Image from 'next/image';
 
 export default function Form({ onAddProduct }) {
 	const {
+		watch,
 		reset,
 		register,
 		handleSubmit,
 		formState: { errors },
 	} = useForm();
 
+	const CLOUD = process.env.CLOUDINARY_CLOUD;
+	const PRESET = process.env.CLOUDINARY_PRESET;
+
+	const placeholderImage = {
+		url: 'https://images.unsplash.com/photo-1542295669297-4d352b042bca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80',
+		width: 120,
+		height: 180,
+	};
+
+	const [previewImage, setPreviewImage] = useState(placeholderImage);
+
+	const uploadImage = async () => {
+		try {
+			const url = `https://api.cloudinary.com/v1_1/${CLOUD}/upload`;
+			const image = watch('image')[0];
+
+			const fileData = new FormData();
+			fileData.append('file', image);
+			fileData.append('upload_preset', PRESET);
+
+			const response = await fetch(url, {
+				method: 'POST',
+				body: fileData,
+			});
+
+			setPreviewImage(await response.json());
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
+
 	const onSubmit = data => {
+		data.image = {
+			url: previewImage.url,
+			width: previewImage.width,
+			height: previewImage.height,
+		};
 		onAddProduct(data);
 		reset();
 		handleClick();
@@ -31,7 +69,7 @@ export default function Form({ onAddProduct }) {
 
 		setTimeout(() => {
 			setButtonText(initialButtonText);
-		}, 1000);
+		}, 2000);
 	};
 
 	return (
@@ -42,16 +80,19 @@ export default function Form({ onAddProduct }) {
 						<Label htmlFor="image">image url</Label>
 						<Input
 							id="image"
-							type="url"
+							type="file"
 							aria-invalid={errors.image ? 'true' : 'false'}
 							{...register('image', {
 								required: true,
 							})}
-							placeholder="Add image URL"
+							onChange={uploadImage}
 						/>
-						{errors.image && errors.image.type === 'required' && (
-							<span>please enter a valid url</span>
-						)}
+						<Image
+							alt="placeholder"
+							src={previewImage.url}
+							width={previewImage.width}
+							height={previewImage.height}
+						/>
 					</InputSingleContainer>
 					<InputSingleContainer>
 						<Label htmlFor="title">title</Label>
