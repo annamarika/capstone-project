f1732d4368af1e6aa233ba0e7c5812ba5ab4288d
Anna Holtmann <anna_maria.heuer@mac.com>
2022-06-04 16:20:52 +0200

    Updating animation

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,19 +1,29 @@
-import styled from 'styled-components';
-import LogoButton from '../Button/LogoButton';
+import { useState } from 'react';
+import styled, { keyframes, css } from 'styled-components';
+
+import LogoButtonStyle from '../UI/Button/LogoButton.styled';
 
 export default function Logo() {
+	const [toggle, setToggle] = useState(false);
+
 	return (
 		<LogoBG>
-			<TopLeft>
-				<TopRightOrange />
-				<TopRightPink />
-			</TopLeft>
-			<Bottom>
-				<BottomWhite>
-					<LogoButton />
+			<TopLeft toggle={toggle} />
+			<Orange toggle={toggle} />
+			<TopRightPink toggle={toggle} />
+
+			<Bottom toggle={toggle}>
+				<BottomWhite toggle={toggle}>
+					<LogoButtonStyle
+						toggle={toggle}
+						type="button"
+						onClick={() => setToggle(!toggle)}
+					>
+						{toggle ? 'OPEN' : 'CLOSE'}
+					</LogoButtonStyle>
 				</BottomWhite>
 			</Bottom>
-			<Legs>
+			<Legs toggle={toggle}>
 				<LegsLeft />
 				<LegsRight />
 			</Legs>
@@ -31,8 +41,36 @@ const LogoBG = styled.div`
 	transform: translate(-50%, -50%);
 	padding: 20px;
 	height: 375px;
-	_width: 100;
 	border-radius: 40px;
+`;
+
+const enter = keyframes`
+from {
+    left: 0px;
+    top: 0px;
+    border-radius: 50% 50% 0 0;
+    
+   
+}
+to {
+    left: -20px;
+    top: 10px;
+    border-radius: 0 50% 0 0;  
+}
+
+`;
+
+const update = keyframes`
+0% {
+    left: -20px;
+    top: 10px;
+    border-radius: 0 50% 0 0;
+}
+100% {
+    left: 0px;
+    top: 0px;
+    border-radius: 50% 50% 0 0;
+}
 `;
 
 const TopLeft = styled.div`
@@ -41,16 +79,66 @@ const TopLeft = styled.div`
 	background-color: #4b587f;
 	border-radius: 50% 50% 0 0;
 	position: relative;
+    animation: ${({ toggle }) =>
+		toggle
+			? css`
+					${update} 0.7s linear forwards
+			  `
+			: css`
+					${enter} 0.7s linear forwards
+			  `}; }
 `;
 
-const TopRightOrange = styled.div`
+const enterOrange = keyframes`
+from {left: 70px;
+top: -155px;}
+to {left: 40px;
+    top: -120px;}
+`;
+
+const updateOrange = keyframes`
+0% {left: 40px;
+    top: -120px;}
+100% {left: 70px;
+    top: -155px;}
+`;
+
+const Orange = styled.div`
 	width: 210px;
 	height: 78px;
 	background-color: #db6c4c;
 	border-radius: 0 38px 38px 0;
-	position: absolute;
+	position: relative;
 	left: 70px;
+	top: -155px;
 	z-index: -10;
+    animation: ${({ toggle }) =>
+		toggle
+			? css`
+					${updateOrange} 0.7s linear forwards
+			  `
+			: css`
+					${enterOrange} 0.7s linear forwards
+			  `}; }
+`;
+
+const enterTopRightPink = keyframes`
+from {left: 70px;
+    border-radius: 38px 38px 0 38px;
+    top: -156px;}
+to {left: 90px;
+    border-radius: 38px 0 0 38px;
+    top: -146px;}
+
+`;
+
+const updateTopRightPink = keyframes`
+0% {left: 90px;
+    border-radius: 38px 0 0 38px;
+    top: -146px;}
+100% {left: 70px;
+    border-radius: 38px 38px 0 38px;
+    top: -156px;}
 `;
 
 const TopRightPink = styled.div`
@@ -58,34 +146,136 @@ const TopRightPink = styled.div`
 	height: 78px;
 	background-color: #e2cadf;
 	border-radius: 38px 38px 0 38px;
-	position: absolute;
+	position: relative;
 	left: 70px;
-	top: 77px;
+	top: -156px;
 	z-index: 10;
+    animation: ${({ toggle }) =>
+		toggle
+			? css`
+					${updateTopRightPink} 0.7s linear forwards
+			  `
+			: css`
+					${enterTopRightPink} 0.7s linear forwards
+			  `}; }
 `;
+
+const enterBottom = keyframes`
+
+from {top: -156px;
+    height: 126px;}
+to {top: -166px;
+    height: 136px;
+}
+`;
+
+const updateBottom = keyframes`
+
+0% {top: -166px;
+    height: 136px}
+100% {top: -156px;
+    height: 126px;}
+`;
+
 const Bottom = styled.div`
 	width: 280px;
 	height: 126px;
 	background-color: #4b587f;
 	position: relative;
+	top: -156px;
+    animation: ${({ toggle }) =>
+		toggle
+			? css`
+					${updateBottom} 0.7s linear forwards
+			  `
+			: css`
+					${enterBottom} 0.7s linear forwards
+			  `}; }
+`;
+
+const enterBottomWhite = keyframes`
+
+from {height: 96px;
+    width: 254px;
+    left: 13px;
+    top: 15px;
+    border-radius: 50px 50px 50px 50px;
+    background-color: #4b587f;
+    }
+to {height: 116px;
+    width: 280px;
+    left: 0px;
+    top: 20px;
+    border-radius: 0;
+    background-color: #4b587f;
+    
+}
+`;
+
+const updateBottomWhite = keyframes`
+
+0% {height: 116px;
+    width: 280px;
+    left: 0px;
+    top: 20px;
+    border-radius: 0;
+    }
+100% {height: 96px;
+    width: 254px;
+    left: 13px;
+    top: 15px;
+    border-radius: 50px 50px 50px 50px;
+    }
 `;
 
 const BottomWhite = styled.div`
 	width: 254px;
 	height: 96px;
-	background-color: white;
+    border: solid 3px #db6c4c;
 	border-radius: 0 50px 0 50px;
 	position: absolute;
 	left: 13px;
 	top: 15px;
-	transition: booky 1s;
-	animation: BottomWhiteAnimation 2s ease-in;
 	display: flex;
 	justify-content: center;
+    animation: ${({ toggle }) =>
+		toggle
+			? css`
+					${updateBottomWhite} 0.7s linear forwards
+			  `
+			: css`
+					${enterBottomWhite} 0.7s linear forwards
+			  `}; }
+`;
+
+const enterLegs = keyframes`
+
+from {top: -156px;
+    }
+to {top: -166px;
+    
+}
+`;
+
+const updateLegs = keyframes`
+
+0% {top: -166px;
+    }
+100% {top: -156px;
+    }
 `;
 
 const Legs = styled.div`
 	position: relative;
+	top: -156px;
+    animation: ${({ toggle }) =>
+		toggle
+			? css`
+					${updateLegs} 0.7s linear forwards
+			  `
+			: css`
+					${enterLegs} 0.7s linear forwards
+			  `}; }
 `;
 
 const LegsLeft = styled.div`
--- src/Components/UI/Button/LogoButton.styled.js
+++ src/Components/UI/Button/LogoButton.styled.js
@@ -1,12 +1,38 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
+
+const enterLogoButtonStyle = keyframes`
+
+from {font-size: 30px;
+    opacity: 0;
+}
+to {font-size: 40px;
+    opacity: 1;
+}
+`;
+
+const updateLogoButtonStyle = keyframes`
+
+0% {font-size: 40px;
+    opacity: 0;}
+100% {font-size: 30px;
+    opacity: 1;}
+`;
 
 const LogoButtonStyle = styled.button`
-	font-size: 50px;
+	font-size: 30px;
 	font-weight: 700;
-	color: var(--text-color-dark);
+	color: white;
 	background-color: transparent;
 	align-item: center;
 	border: none;
+    animation: ${({ toggle }) =>
+		toggle
+			? css`
+					${updateLogoButtonStyle} 0.7s linear forwards
+			  `
+			: css`
+					${enterLogoButtonStyle} 0.7s linear forwards
+			  `}; }
 `;
 
 export default LogoButtonStyle;
