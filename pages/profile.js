dd564cc83a06c8e4b9281fea78bdd7b630816383
Anna Holtmann <anna_maria.heuer@mac.com>
2022-06-06 12:08:43 +0200

    Toggle login

--- pages/profile.js
+++ pages/profile.js
@@ -2,6 +2,7 @@ import { getProducts } from '../src/services/get-products';
 import ProductGrid from '../src/Components/Product/ProductGrid';
 import { swrFetcher } from '../src/Components/lib/swr-fetcher';
 import { SWRConfig } from 'swr';
+import { useSession } from 'next-auth/react';
 
 export async function getStaticProps() {
 	const products = await getProducts();
@@ -15,10 +16,11 @@ export async function getStaticProps() {
 	};
 }
 
-export default function Products({ fallback }) {
+export default function Products({ fallback }, user) {
+	const { data: session } = useSession();
 	return (
 		<SWRConfig value={{ fetcher: swrFetcher, fallback }}>
-			<ProductGrid />
+			{session && session.user.email !== user.email && <ProductGrid />}
 		</SWRConfig>
 	);
 }
--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import AccountButton from '../Button/AccountButton';
 import Bottom from '../UI/Logo/LogoBottom.styled';
 import BottomDecorate from '../UI/Logo/LogoBottomDecorate.styled';
 import LogoContainer from '../UI/Logo/LogoContainer.styled';
@@ -8,10 +7,34 @@ import LegsContainer from '../UI/Logo/LogoLegsContainer.styled';
 import Orange from '../UI/Logo/LogoOrange.styled';
 import Pink from '../UI/Logo/LogoPink.styled';
 import TopLeft from '../UI/Logo/LogoTopLeft.styled';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import LogoButtonStyle from '../UI/Button/LogoButton.styled';
 
 export default function Logo() {
 	const [toggle, setToggle] = useState(true);
+	const { data: session } = useSession();
 
+	if (session) {
+		return (
+			<LogoContainer>
+				<TopLeft toggle={!toggle} />
+				<Orange toggle={!toggle} />
+				<Pink toggle={!toggle} />
+
+				<Bottom toggle={!toggle}>
+					<BottomDecorate toggle={!toggle}>
+						<LogoButtonStyle type="button" onClick={() => signOut(setToggle(toggle))}>
+							SIGN OUT
+						</LogoButtonStyle>
+					</BottomDecorate>
+				</Bottom>
+				<LegsContainer toggle={!toggle}>
+					<Legs />
+					<Legs variant="legRight" />
+				</LegsContainer>
+			</LogoContainer>
+		);
+	}
 	return (
 		<LogoContainer>
 			<TopLeft toggle={toggle} />
@@ -20,11 +43,9 @@ export default function Logo() {
 
 			<Bottom toggle={toggle}>
 				<BottomDecorate toggle={toggle}>
-					<AccountButton
-						toggle={toggle}
-						type="button"
-						onClick={() => setToggle(!toggle)}
-					/>
+					<LogoButtonStyle type="button" onClick={() => signIn(setToggle(!toggle))}>
+						SIGN IN
+					</LogoButtonStyle>
 				</BottomDecorate>
 			</Bottom>
 			<LegsContainer toggle={toggle}>
